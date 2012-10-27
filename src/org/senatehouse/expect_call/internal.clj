e5151428ed690f85833457822f6846ce435b5417
Meredydd Luff <meredydd@senatehouse.org>
2012-10-27 20:53:52 +0100

    All tests from README pass

---
+++ src/org/senatehouse/expect_call/internal.clj
@@ -0,0 +1,117 @@
+(ns org.senatehouse.expect-call.internal
+  (use clojure.test [clojure.core.match :only (match)]))
+
+(def ^:dynamic *disable-interception* false)
+
+(defn my-report
+  "Disable interception, to prevent looping if the test reporting code
+   uses a function we're intercepting. Also put accurate file and line
+   information into the message."
+  [msg depth]
+
+  (binding [*disable-interception* true]
+    (report
+     (merge
+      (let [stack-trace (.getStackTrace (new Throwable))
+            ^StackTraceElement s (nth stack-trace (inc depth))]
+        {:file (.getFileName s) :line (.getLineNumber s)
+         :stack-trace (seq stack-trace)})
+      msg))))
+
+
+(defn -expected-call
+  "Used by (expect-call) macro. You don't call this."
+  [[more-fns calls :as state] real-fn real-fn-name args]
+  (if *disable-interception*
+    (apply real-fn args)
+
+    (let [[ex-real-fn ex-fn ex-real-fn-name ex-args] (first @calls)]
+      (if (= real-fn ex-real-fn)
+        (do ; It matched the next explicit expectation. Run it.
+          (swap! calls rest)
+          (apply ex-fn args))
+        
+        ;; It didn't match an explicit expectation - did it match
+        ;; a :more or :never?
+        (if-let [more-fn (more-fns real-fn)]
+          (apply more-fn args)
+          
+          ;; Nope - it's just wrong
+          (my-report {:type :fail
+                      :message (if ex-real-fn
+                                 "Wrong function called"
+                                 (str "Too many calls to " real-fn-name))
+                      :expected (cons ex-real-fn-name ex-args)
+                      :actual (cons real-fn-name args)} 3))))))
+
+(defn make-mock [[tags real-fn-name & [args & body]]]
+  (let [args (or args '[& _])
+        real-fn (gensym "real-fn")]
+    `(let [~real-fn ~real-fn-name]
+       (fn ~(gensym (str real-fn-name "-mock")) [& ~'myargs]
+         (match (apply vector ~'myargs)
+                ~args (do ~@body ~@(when (:do tags) `((apply ~real-fn ~'myargs))))
+                :else (my-report {:type :fail
+                                  :message "Unexpected arguments"
+                                  :expected (quote ~(cons real-fn-name args))
+                                  :actual (cons (quote ~real-fn-name)
+                                                ~'myargs)} 6))))))
+
+(defmacro -expect-call
+  "expected-fns: (fn arg-match body...)
+                 or [(fn arg-match body...), (fn arg-match body...)...]
+   Each fn may be preceded by keywords :more, :never or :do."
+  [expected-fns & body]
+  
+  (let [expected-fns (if (vector? expected-fns) expected-fns [expected-fns])
+        expected-fns (for [fspec expected-fns]
+                       (cons (apply hash-set (take-while keyword? fspec))
+                             (drop-while keyword? fspec)))
+
+        state (gensym "state")]
+
+    `(let [;; Format: {function closure, function closure}
+           more-fns#
+           ~(apply merge {}
+                   (for [[tags real-fn :as expected-fn] expected-fns
+                         :when (or (:more tags) (:never tags))]
+                     (if (:more tags)
+                       {real-fn (make-mock expected-fn)}
+                       {real-fn `(fn [& args#]
+                                   (my-report {:type :fail
+                                               :message ~(str real-fn " should not be called")
+                                               :expected (quote (:never ~real-fn))
+                                               :actual (cons (quote ~real-fn)
+                                                             args#)} 4))})))
+       
+       
+           ;; Format: ([function closure fn-name arg-form],
+           ;;          [function closure arg-form], ...)
+           calls# (atom
+                   (list
+                    ~@(for [[tags real-fn args :as expected-fn] expected-fns
+                            :when (not (or (:more tags) (:never tags)))]
+                        [real-fn (make-mock expected-fn)
+                         `(quote ~real-fn) `(quote ~args)])))
+
+           ~state [more-fns# calls#]]
+       
+       (let [result#
+             (with-redefs
+               ~(apply vector
+                       (let [fns (reduce (fn [set [_ real-fn]] (conj set real-fn))
+                                         #{} expected-fns)]
+                         (apply
+                          concat
+                          (for [f fns]
+                            [f `(let [f# ~f]
+                                  (fn ~(symbol (str (name f) "-mock")) [& a#]
+                                    (-expected-call ~state f# (quote ~f) a#)))]))))
+               ~@body)]
+         ;; If we haven't used up all our calls, we error out
+         (when-let [[_# _# ex-fn-name# ex-args#] (first @calls#)]
+           (my-report {:type :fail
+                       :message (str "Function " ex-fn-name# " was not called")
+                       :expected (cons ex-fn-name# ex-args#)
+                       :actual nil} 0))
+         result#))))
--- test/org/senatehouse/expect_call_test.clj
+++ test/org/senatehouse/expect_call_test.clj
@@ -9,6 +9,41 @@
   (apply println args)
   :logged)
 
+(defn check-error [a b]
+  (when (= a :error)
+    (log "ERROR:" (pr-str b))))
+
+
+(defmacro expecting-failure
+  "Execute body, expecting it to report a test failure.
+
+   It would be neat to implement this as:
+
+   (with-expect-call ~'(report [{:type :fail}])
+     ~@body)
+
+   ...however, expect-call disables its interception hooks
+   while calling (report), to avoid going into an infinite
+   loop. So it doesn't work in this case. Sadly."
+  [& body]
+  `(let [reported?# (atom false)]
+     (with-redefs [report (fn [m#]
+                            (when-not m#
+                              (throw (Exception. "(report) requires a parameter")))
+                            (swap! reported?# #(or % m#)))]
+       (try
+         ~@body
+         (finally
+          (cond
+           (not @reported?#) (report {:type :fail,
+                                      :expected {:type :fail},
+                                      :actual nil,
+                                      :message "Expected test to fail"})
+           (not= (:type @reported?#) :fail) (report @reported?#)
+
+           :else :ok))))
+     @reported?#))
+
 
 (deftest mocks
   (let [make-mock make-mock
@@ -17,23 +52,14 @@
     
     (is (= (mock :error "abc") :return-value))
 
-    (expect-call (report) (mock :not-an-error "abc"))
+    (expecting-failure
+      (mock :not-an-error "abc"))
 
     (is (= (do-mock :error "abc") :logged) ":do mocks actually call the function")
 
     :ok))
 
 
-
-(defn check-error [a b]
-  (when (= a :error)
-    (log "ERROR:" (pr-str b))))
-
-(defmacro expecting-failure
-  "Execute body, expecting it to report a test failure"
-  [& body]
-  `(with-expect-call ~'(report [{:type :fail}])
-     ~@body))
 
 (deftest readme-examples
 
@@ -53,18 +79,44 @@
 
   (testing "Enforce multiple calls"
     (expecting-failure
-     (with-expect-call [(log ["ERROR:" "abc"])
-                        (log ["ERROR:" "xyz"])]
+     (with-expect-call [(log ["ERROR:" "\"abc\""])
+                        (log ["ERROR:" "\"xyz\""])]
        (check-error :error "abc")
        (check-error :error "xyz")
        (check-error :error "Surprise!"))))
 
   (testing "Multiple calls"
-    (with-expect-call [(log ["ERROR:" "abc"])
-                       (log ["ERROR:" "xyz"])]
+    (with-expect-call [(log ["ERROR:" "\"abc\""])
+                       (log ["ERROR:" "\"xyz\""])]
       (check-error :error "abc")
-      (check-error :error "xyz")))
+      (check-error :error "xyz"))))
 
 
+(defmacro check-line [expr]
+  `(let [report# (expecting-failure ~expr)
+         ~'file-and-line (str (:file report#) ":" (:line report#))]
+     (is (~'= ~'file-and-line ~(str "expect_call_test.clj:" (:line (meta expr)))))
+     (when-not (= ~'file-and-line ~(str "expect_call_test.clj:" (:line (meta expr))))
+       (println "Actual report:" (pr-str report#))
+       (println "Actual stack trace:")
+       (doseq [s# (take 10 (:stack-trace report#))]
+         (println s#)))))
+
+(deftest line-number-reporting
+  ;; Test that every (report) mode we have yields the correct
+  ;; line number
+
+  (testing ":never"
+    (check-line
+     (with-expect-call (:never log) (log :test))))
   
-)
+  (testing "Not called"
+    (check-line
+     (with-expect-call (log))))
+  
+  (testing "Wrong function"
+    (check-line (with-expect-call [(log) (println)] (println "hi"))))
+  
+  (testing "Wrong args"
+    (check-line (with-expect-call (log [:x]) (log :y)))))
+
