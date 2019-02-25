0ed1df7063408f396ad95cb77201e052d53fec7c
Meredydd Luff <meredydd@anvil.works>
2019-02-25 16:12:50 +0000

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject org.senatehouse/expect-call "0.1.0"
+(defproject org.senatehouse/expect-call "0.2.0"
   :description "A Clojure library for no-fuss function mocking"
   :url "https://github.com/meredydd/expect-call"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/core.match "0.2.0-alpha11"]])
+                 [org.clojure/core.match "0.2.0-alpha11"]]
+  :deploy-repositories [["clojars" {:sign-releases false :url "https://clojars.org/repo"}]])
