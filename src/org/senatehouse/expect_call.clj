dc1d15f4ae8e3cd9b8b9b7945a48cd12f1bb40af
Imre Kószó <imrekoszo@users.noreply.github.com>
2025-02-14 17:54:49 +0100

    add more docs to with-expect-call alias

--- src/org/senatehouse/expect_call.clj
+++ src/org/senatehouse/expect_call.clj
@@ -6,12 +6,16 @@
                  or [(fn arg-match body...), (fn arg-match body...)...]
    Each fn may be preceded by keywords :more, :never or :do."
   [expected-fns & body]
-  
+
   `(internal/-expect-call ~expected-fns ~@body))
 
 ;; This is an alias
 (defmacro with-expect-call
-  "This is an alias for expect-call, with a with- prefix
+  "expected-fns: (fn arg-match body...)
+                 or [(fn arg-match body...), (fn arg-match body...)...]
+   Each fn may be preceded by keywords :more, :never or :do.
+
+   This is an alias for expect-call, with a with- prefix
    so emacs clojure-mode indents it more nicely"
-  [& args]
-  `(internal/-expect-call ~@args))
+  [expected-fns & body]
+  `(internal/-expect-call ~expected-fns ~@body))
