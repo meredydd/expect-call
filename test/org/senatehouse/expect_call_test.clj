7dfe0cb193ea436dcb1db1c4ea9c2136f25666ea
Imre Kószó <imrekoszo@users.noreply.github.com>
2025-02-14 18:09:10 +0100

    add more tests to verify clj-kondo hooks

--- test/org/senatehouse/expect_call_test.clj
+++ test/org/senatehouse/expect_call_test.clj
@@ -13,6 +13,7 @@
   (when (= a :error)
     (log "ERROR:" (pr-str b))))
 
+(defn destructuring [_x])
 
 (defmacro expecting-failure
   "Execute body, expecting it to report a test failure.
@@ -93,8 +94,95 @@
     (sut/with-expect-call [(log ["ERROR:" "\"abc\""])
                            (log ["ERROR:" "\"xyz\""])]
       (check-error :error "abc")
-      (check-error :error "xyz"))))
+      (check-error :error "xyz")))
 
+  (testing "arg checking against binding"
+    (let [foo ":foo"]
+      (sut/with-expect-call (log ["ERROR:" foo])
+                            (check-error :error :foo))
+      (expecting-failure
+        (sut/with-expect-call (log ["ERROR:" foo])
+                              (check-error :error :bar)))))
+
+  (testing "sequence destructuring"
+    (sut/with-expect-call
+     (destructuring [[a b]] (is (= a b)))
+     (destructuring (mapv inc [3 3])))
+
+    (expecting-failure
+     (sut/with-expect-call
+      (destructuring [[a b]] (is (= a b)))
+      (destructuring (mapv inc [3 4]))))
+
+    (sut/with-expect-call
+     (destructuring [[a [b [c :foo]]]] (is (= a b c)))
+     (destructuring [1 [1 [1 :foo]]]))
+
+    ;; :as not supported
+    #_(sut/with-expect-call
+     (destructuring [[a b :as c]] (is (= [4 4] c)))
+     (destructuring (mapv inc [3 3])))
+
+    (testing "with arg checking against binding"
+      (let [foo :foo]
+        (sut/with-expect-call
+         (destructuring [[foo bar]] (is (= foo bar)))
+         (destructuring [:foo :foo]))
+
+        (expecting-failure
+         (sut/with-expect-call
+          (destructuring [[foo bar]] (is (= foo bar)))
+          (destructuring [:bar :bar])))
+
+        (sut/with-expect-call
+         (destructuring [[foo :bar]])
+         (destructuring [:foo :bar]))
+
+        (expecting-failure
+         (sut/with-expect-call
+          (destructuring [[foo :bar]])
+          (destructuring [:foo :foo])))
+
+        (expecting-failure
+         (sut/with-expect-call
+          (destructuring [[foo :bar]])
+          (destructuring [:bar :bar])))))
+
+    ;; map destructuring not allowed
+    #_(sut/with-expect-call
+       (destructuring [{foo :foo}] (is (= foo 1)))
+       (destructuring [{:foo 1 :bar 1}]))
+    #_(expecting-failure
+       (sut/with-expect-call
+        (destructuring [{foo :foo bar :bar}] (is (= foo bar)))
+        (destructuring {:foo 1 :bar 2}))))
+
+  (testing "matching literal vectors and maps"
+    (sut/with-expect-call
+     (destructuring [[1 1]])
+     (destructuring (mapv inc [0 0])))
+
+    (sut/with-expect-call
+     (destructuring [[nil nil]])
+     (destructuring (into [] (repeat 2 nil))))
+
+    (sut/with-expect-call
+     (destructuring [[:foo :bar]])
+     (destructuring [:foo :bar]))
+
+    (expecting-failure
+     (sut/with-expect-call
+      (destructuring [[1 1]])
+      (destructuring (mapv inc [0 3]))))
+
+    (sut/with-expect-call
+     (destructuring [{:foo :bar}])
+     (destructuring (zipmap [:foo] [:bar])))
+
+    (expecting-failure
+     (sut/with-expect-call
+      (destructuring [{:foo :bar}])
+      (destructuring (zipmap [:foo] [:qux]))))))
 
 (defmacro check-line [expr]
   `(let [report# (expecting-failure ~expr)
